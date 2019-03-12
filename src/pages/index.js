9e6a3b0ae453cfefbd4291704c5660ee7e063c14
Anton Stepan <antonstepan@google.com>
2019-03-12 12:12:11 +0000

    Add social media links. Make the color random. #bestuseofjavascript2019

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,21 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const linkStyle = {
+  fontSize: `0.8rem`,
+  fontWeight: `bold`,
+  margin: `0 5px`,
+};
+
+const colors = [
+  "#009933", "#006699", "#33cccc", "#99cc00", "#f60", "crimson", "darkolivegreen",
+  "lightseagreen", "deepskyblue", "midnightblue", "darkviolet", "darkslategray",
+];
+
+const getColor = () => {
+  return colors[Math.floor(Math.random() * colors.length)];
+};
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Hello Anton" keywords={[`antonstepan`,]} />
@@ -15,6 +30,12 @@ const IndexPage = () => (
         color: 'black'
       }}>Anton Stepan</h1>
       <span>How's it going?</span>
+      <p style={{marginTop: `10px`}}>
+        <a style={{...linkStyle, color: getColor()}} href="" target="_blank">INSTAGRAM</a>
+        <a style={{...linkStyle, color: getColor()}} href="" target="_blank">TWITTER</a>
+        <a style={{...linkStyle, color: getColor()}} href="" target="_blank">LINKED IN</a>
+        <a style={{...linkStyle, color: getColor()}} href="" target="_blank">FACEBOOK</a>
+      </p>
     </div>
   </Layout>
 )
