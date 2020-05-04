dc58777759b74d7cfec78425c9b5b1d2c43d7d10
Anton Stepan <info@antonstepan.it>
2020-05-04 10:02:47 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,7 @@ const IndexPage = () => (
       <p style={{marginTop: `10px`}}>
         <a style={{...linkStyle, color: getColor()}} href="https://www.instagram.com/helloanton.xyz/" target="_blank">INSTAGRAM</a>
         <a style={{...linkStyle, color: getColor()}} href="https://twitter.com/asndev" target="_blank">TWITTER</a>
-        <a style={{...linkStyle, color: getColor()}} href="https://www.linkedin.com/in/antonstepan/" target="_blank">LINKED IN</a>
+        <a style={{...linkStyle, color: getColor()}} href="https://www.linkedin.com/in/antonstepan/" target="_blank">LINKEDIN</a>
         <a style={{...linkStyle, color: getColor()}} href="https://www.facebook.com/anton.stepan" target="_blank">FACEBOOK</a>
       </p>
     </div>
